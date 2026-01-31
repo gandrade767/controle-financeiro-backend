92f4318dc0bd0e7368421b275b460976b42f8d74
gandrade767 <gandrade767@gmail.com>
2026-01-30 23:26:40 -0400

    feature status account

--- src/modules/accounts/accounts.routes.js
+++ src/modules/accounts/accounts.routes.js
@@ -15,6 +15,9 @@ router.get('/:id', accountsController.getAccountById);
 // Atualizar conta
 router.put('/:id', accountsController.updateAccount);
 
+// Status conta
+router.patch("/:id/toggle-status", accountsController.toggleAccountStatus);
+
 // Excluir conta
 router.delete('/:id', accountsController.deleteAccount);
 
